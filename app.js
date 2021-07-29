ea240741cdd7afdfdd693b52e503f8987b4020b9
Your Name <leobarrientos02@gmail.com>
2021-07-29 17:20:54 -0400

    PopUp functions added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const colorDivs = document.querySelectorAll(".color");
 const generateBtn = document.querySelector(".generate");
 const sliders = document.querySelectorAll("input[type='range");
 const currentHexes = document.querySelectorAll(".color h2");
+const popup = document.querySelector(".copy-container");
 let initialColors;
 
 // Event listeners
@@ -14,6 +15,16 @@ colorDivs.forEach((div, index) => {
   div.addEventListener("change", () => {
     updateTestUI(index);
   });
+});
+currentHexes.forEach((hex) => {
+  hex.addEventListener("click", () => {
+    copyToClipboard(hex);
+  });
+});
+popup.addEventListener("transitionend", () => {
+  const popupBox = popup.children[0];
+  popup.classList.remove("active");
+  popupBox.classList.remove("active");
 });
 
 //Functions
@@ -158,6 +169,20 @@ function resetInputs() {
       slider.value = Math.floor(satValue * 100) / 100;
     }
   });
+} //Ends resetInputs() function
+function copyToClipboard(hex) {
+  const el = document.createElement("textarea");
+  el.value = hex.innerText;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand("copy");
+  document.body.removeChild(el);
+
+  // Pop up animation
+  const popupBox = popup.children[0];
+  popup.classList.add("active");
+  popupBox.classList.add("active");
 }
+
 //Function Call
 randomColors();
