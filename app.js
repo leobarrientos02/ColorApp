ce5ca7db8d9825a9abe9119a29c6b9a21465b045
Your Name <leobarrientos02@gmail.com>
2021-07-31 12:11:03 -0400

    Update
    Save Popup design

--- app.js
+++ app.js
@@ -10,6 +10,9 @@ const closeAdjustments = document.querySelectorAll(".close-adjustment");
 const sliderContainers = document.querySelectorAll(".sliders");
 let initialColors;
 
+//Local Storage
+let savedPalettes = [];
+
 // Event listeners
 generateBtn.addEventListener("click", randomColors);
 sliders.forEach((slider) => {
@@ -239,5 +242,39 @@ function lockLayer(e, index) {
     e.target.innerHTML = '<i class="fas fa-lock-open"></i>';
   }
 } // Ends lockLayer(e,index)
+
+//Implement Save to palatte and Local Storage
+const saveBtn = document.querySelector(".save");
+const submitSave = document.querySelector("submit-save");
+const closeSave = document.querySelector(".close-save");
+const saveContainer = document.querySelector(".save-container");
+const saveInput = document.querySelector(".save-container input");
+
+// Event Listeners
+saveBtn.addEventListener("click", openPalette);
+closeSave.addEventListener("click", closePalette);
+
+function openPalette(e) {
+  const popup = saveContainer.children[0];
+  saveContainer.classList.add("active");
+  popup.classList.add("active");
+} // Ends openPalette(e)
+
+function closePalette(e) {
+  const popup = saveContainer.children[0];
+  saveContainer.classList.remove("active");
+  popup.classList.remove("active");
+} // Ends closePalette(e)
+
+function savePalette(e) {
+  saveContainer.classList.remove("active");
+  popup.classList.remove("active");
+  const name = saveInput.value;
+  const colors = [];
+  currentHexes.forEach((hex) => {
+    colors.push(hex.innerText);
+  });
+}
+
 //Function Call
 randomColors();
